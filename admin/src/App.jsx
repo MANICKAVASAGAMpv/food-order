6feab5b442319a648dfd5cfbfef928b0ccfa037a
MANICKAVASAGAM P V <110043525+MANICKAVASAGAMpv@users.noreply.github.com>
2024-09-24 17:30:09 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 
 const App = () => {
-    const url =  'http://localhost:4000'
+    const url =  'https://food-order-backend5-gemk.onrender.com'
   return (
     <div>
       <ToastContainer/>
